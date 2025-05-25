93208390578ffdf13a008afbc7b51f39a72450d8
mdrifat59 <mdrifatulislam59@gmail.com>
2025-05-25 14:07:10 +0600

    3d-background-box is done

--- 3d-background-boxs/script.js
+++ 3d-background-boxs/script.js
@@ -1,1 +1,21 @@
- 
+document.addEventListener("DOMContentLoaded", function () {
+    const boxes = document.querySelector(".boxes")
+    const button = document.querySelector("#magicbtn")
+
+    function createBox() {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                const box = document.createElement("div")
+                box.classList.add("box")
+                box.style.backgroundPosition = `${-125 * j}px ${-125 * i}px`
+                boxes.appendChild(box)
+            }
+        }
+    };
+    createBox()
+    button.addEventListener("click", function () {
+        boxes.classList.toggle("big")
+    })
+
+
+})
